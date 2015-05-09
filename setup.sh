ef1312eedf14289fdf477b9c7aff39c15cdb2035
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-09 08:11:33 -0700

    name bosh target concourse-bosh-lite

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ vagrant up --provider=aws
 vagrant ssh -c 'sudo iptables -t nat -A PREROUTING -p tcp -d $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4) --dport 8080 -j DNAT --to 10.244.8.2:8080'
 
 IP=$(vagrant ssh-config | grep HostName | awk '{print $2}')
-yes admin | bosh target $IP
+yes admin | bosh target $IP concourse-bosh-lite
 
 bosh upload release https://bosh.io/d/github.com/concourse/concourse
 bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release
